77e7f2d87974f889927a54f15168c82471246fce
V. Karthik <karthikv81291@gmail.com>
2025-01-07 12:18:27 +0530

    Add files via upload

--- Entertainment/Video Streaming Platform/app/forms/forms.py
+++ Entertainment/Video Streaming Platform/app/forms/forms.py
@@ -1,1 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, FileField, SubmitField
+from wtforms.validators import DataRequired
 
+class VideoForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    file = FileField('File', validators=[DataRequired()])
+    submit = SubmitField('Upload')
