25fd2559a1a7f893e9ab7035a8607040e58c77ac
V. Karthik <karthikv81291@gmail.com>
2025-01-07 11:44:35 +0530

    Delete Data Analysis and Visualization directory

--- Data Analysis and Visualization/api_client.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def get_aqi_data(city, country):
-    url = f"https://api.openaq.org/v1/latest?city={city}&country={country}"
-    response = requests.get(url)
-    return response.json()
